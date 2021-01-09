c12a1ceed37afbae9f12605b44f59da90edcca5e
clair13 <theclairvoyant1313@yahoo.gr>
2021-01-09 19:48:59 +0200

    Add puzzle input and description

---
+++ day3b/day_3b.rb
@@ -0,0 +1,11 @@
+input = File.readlines('puzzle_input.txt').map { |line| line.chomp }
+
+
+slopes = [[1,1],[3,1],[5,1],[7,1],[1,2]]
+
+
+def total_number_of_trees_encountered(input)
+
+end
+
+total_number_of_trees_encountered(input)
